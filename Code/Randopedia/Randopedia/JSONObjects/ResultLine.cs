67c201f84b0b81d43f2b38f7eb661b4bff8ecf72
thibault.balaye <thibault.balaye@gmail.com>
2013-02-07 19:33:53 +0100

    - Mise en place de messages explicites sur l'interface. - Déplacement des classes JSON dans un répertoire dédié. - Traitement de certains cas d'erreur dans la récupération des résultats de recherche. - Utilisation du fichier de configuration pour l'adresse du serveur de recherche.

---
+++ Code/Randopedia/Randopedia/JSONObjects/ResultLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Randopedia.JSONObjects
+{
+    public class ResultLine
+    {
+        public string Xpath  { get; set; }
+        public string Content { get; set; }
+    }
+}
