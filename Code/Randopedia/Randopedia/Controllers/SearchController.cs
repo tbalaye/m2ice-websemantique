126a6b77a8782d2c3731dcba1d36f0ea19e2e7f9
thibault.balaye <thibault.balaye@gmail.com>
2013-02-07 11:29:48 +0100

    Ajout du front-end "Randopedia"

---
+++ Code/Randopedia/Randopedia/Controllers/SearchController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Randopedia.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        //
+        public ActionResult Index()
+        {
+            ViewBag.Result = "";
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("PartialSearch");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        //
+        // POST: /Search/SearchRando/
+        //
+        [HttpPost]
+        public ActionResult SearchRando(string SearchString)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                ViewBag.Result = Regex.Split(SearchString, @"[ ]+");
+                return PartialView("PartialSearch");
+            }
+            else
+            {
+                ViewBag.Result = "";
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Code/Randopedia/Randopedia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Randopedia.MvcApplication" Language="C#" %>
