44a6d99e84891f048ec95fb5830189b55bf11743
Morgan <morgan.moulet@gmail.com>
2013-02-07 19:12:43 +0100

    Ajout webservice de test.

--- Code/Webservice/Search.rb
+++ Code/Webservice/Search.rb
@@ -2,13 +2,19 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
-get '/search/:label' do
-	label = params[:label]
+
+def foo(request)
+	return {Request: request, Results: [{Xpath:"/doc[2]/sec[1]/p[1]", Content:"J'aime la montagne"},{Xpath:"/doc[2]/sec[1]/p[2]", Content:"La montagne est ton amis"},{Xpath:"/doc[3}/sec[2]/p[3]", Content:"La marmotte aime la montagne"}], Precision: "0.3", Rappel: "0.7"}.to_json
+end #foo
+
+get '/search/:request' do
+	request = params[:request]
 	
-	if label.nil?
+	if request.nil?
 		status 404
 	else
 		status 200
-		{method: label, version: "1.00"}.to_json
+		
+		return foo(request)
 	end #if
 end #get '/search/:label'
