2bfabceef260539768bb7ebb6b0a27683b6f0214
Morgan <morgan.moulet@gmail.com>
2013-02-10 20:31:53 +0100

    Modification message retour du webservice. Ajout d'un mode avec Ontologie.

---
+++ Code/Ontologie/Constant.rb
@@ -0,0 +1,5 @@
+
+# Ontologie
+URL_ONTOLOGIE = "172.31.190.49"
+PORT_ONTOLOGIE = "3030"
+ONTOLOGY_FILE_PATH = './ontologies/Baladefusion.owl'
--- Code/Webservice/Search.rb
+++ Code/Webservice/Search.rb
@@ -3,18 +3,24 @@ require 'sinatra'
 require 'json'
 
 
-def foo(request)
-	return {Request: request, Results: [{Xpath:"/doc[2]/sec[1]/p[1]", Content:"J'aime la montagne"},{Xpath:"/doc[2]/sec[1]/p[2]", Content:"La montagne est ton amis"},{Xpath:"/doc[3}/sec[2]/p[3]", Content:"La marmotte aime la montagne"}], Precision: "0.3", Rappel: "0.7"}.to_json
+
+def foo(request, is_with_ontologie)
+	return {Request: request, Results: [{Xpath:"/doc[2]/sec[1]/p[1]", Content:"J'aime la montagne"},{Xpath:"/doc[2]/sec[1]/p[2]", Content:"La montagne est ton amis"},{Xpath:"/doc[3}/sec[2]/p[3]", Content:"La marmotte aime la montagne"}], Precision: "0.3", Rappel: "0.7", Ontologie: is_with_ontologie.to_s, TimeCompute:"1.3"}.to_json
 end #foo
 
-get '/search/:request' do
-	request = params[:request]
-	
+def compute(request, is_with_ontologie)
 	if request.nil?
 		status 404
 	else
-		status 200
-		
-		return foo(request)
+		status 200	
+		return foo(request, is_with_ontologie)
 	end #if
+end #compute
+
+get '/Search/:request' do
+	return compute(params[:request], false)
+end #get '/search/:label'
+
+get '/SearchOntologie/:request' do
+	return compute(params[:request], true)
 end #get '/search/:label'
