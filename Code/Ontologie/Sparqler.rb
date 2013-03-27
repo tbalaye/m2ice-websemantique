249cd9faea7c55a23b45f77d4aaeed03adfaf0fc
thibault.balaye <thibault.balaye@gmail.com>
2013-03-27 22:20:35 +0100

    Mise en place de la récupération des termes de l'ontologie : synonymes, fils et parents.

--- Code/Ontologie/Sparqler.rb
+++ Code/Ontologie/Sparqler.rb
@@ -10,38 +10,64 @@ class Sparqler
 	attr_reader :client
 
 	def initialize
-		@client = SPARQL::Client.new("http://" + URL_ONTOLOGIE + ":" + PORT_ONTOLOGIE + "/balades/query") 
+		@client = SPARQL::Client.new("http://" + URL_ONTOLOGIE + ":" + PORT_ONTOLOGIE + "/balades/query")
 	end
+  
+  def get_synonymes(term)
+    request = "SELECT ?title WHERE { ?subject rdfs:label ?title; rdfs:label \"" + term + "@fr\" }"
+    return query(request)
+  end
+  
+  def get_children(term)
+    request = "
+    SELECT ?label WHERE {
+      ?s rdfs:label ?titre; rdfs:label \"" + term + "\"@fr.
+      ?child rdfs:subClassOf ?s.
+      ?child rdfs:label ?label.
+    }"
+    return query(request)
+  end
+  
+  def get_instances(term)
+    request = "
+    SELECT ?parent WHERE {
+      ?instance rdf:type ?type;
+        rdfs:label ?parent.
+      ?type rdfs:subClassOf ?class.
+      ?class rdfs:label ?label.
+      FILTER (?label != ?parent && regex(?label, \"" + term + "\", \"i\"))
+    }"
+    return query(request)
+  end
+  
+  def query(request)
+    query = self.client.query("
+      PREFIX :        <http://ontologies.alwaysdata.net/space#>
+      PREFIX rdf:      <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs:     <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX owl:      <http://www.w3.org/2002/07/owl#>
+      PREFIX xsd:      <http://www.w3.org/2001/XMLSchema#>
+      PREFIX bal:     <http://www.semanticweb.org/ontologies/2011/11/Balade#>
+    " + request)
+    result = []
+    query.each_solution do |solution|
+      result << solution.inspect.force_encoding("ASCII-8BIT")[/(?<=\")(.*)(?=\")/]
+    end
+    return result
+  end
 end
 
-sparqler = Sparqler.new
+if __FILE__ == $0
+  sparqler = Sparqler.new
+  results = sparqler.get_synonymes("montagne")
+  results.each { |result| puts result + "\n" }
+  puts "\n"
 
-# SELECT * WHERE { ?s ?p ?o } OFFSET 100 LIMIT 10
+  results = sparqler.get_children("montagne")
+  results.each { |result| puts result + "\n" }
+  puts "\n"
 
-query = sparqler.client.select.where([:s, :p, :o]).limit(20)
-
-query.prefix(": <http://ontologies.alwaysdata.net/space#>")
-query.prefix("rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>")
-query.prefix("rdfs: <http://www.w3.org/2000/01/rdf-schema#>")
-query.prefix("owl:  <http://www.w3.org/2002/07/owl#>")
-query.prefix("xsd:  <http://www.w3.org/2001/XMLSchema#>")
-
-query.each_solution do |solution|
-	puts solution.inspect + "\n" *3
+  results = sparqler.get_instances("montagne")
+  results.each { |result| puts result + "\n" }
+  puts "\n"
 end
-
-=begin
-
-PREFIX : <http://ontologies.alwaysdata.net/space#>
-PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX owl:  <http://www.w3.org/2002/07/owl#>
-PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
-
-SELECT *
-WHERE {
-	  ?subject ?property ?object.
-}
-LIMIT 20
-
-=end
