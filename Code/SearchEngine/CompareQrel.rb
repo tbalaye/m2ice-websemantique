8698b18e5c357c00e82611ccf037457e880fdfa5
Morgan <morgan.moulet@gmail.com>
2013-03-27 23:59:57 +0100

    Ajout du rappel et de la précision.

--- Code/SearchEngine/CompareQrel.rb
+++ Code/SearchEngine/CompareQrel.rb
@@ -10,9 +10,13 @@ class CompareQrel
 	def compare(qrels, paragraphes)
 		found = 0
 		error = 0
+		rappel = 0.0
+		precision = 0.0
+		nb_paragraphes = paragraphes.count
+		nb_qrels = qrels.count
 		
-		puts "il y a " + paragraphes.count.to_s + " paragraphes"
-		puts "il y a " + qrels.count.to_s + " qrels"
+		puts "il y a " + nb_paragraphes.to_s + " paragraphes"
+		puts "il y a " + nb_qrels.to_s + " qrels"
 		
 		paragraphes.each do |paragraphe, index|
 			if qrels.detect{|qrel| (qrel["path_file"] == paragraphe[:pathFile].sub("../", "")) and (qrel["xpath"] == paragraphe[:xpath])}
@@ -22,11 +26,16 @@ class CompareQrel
 			end #if
 		end #each
 		
+		rappel = found.to_f / nb_qrels.to_f
+		precision = found.to_f / nb_paragraphes.to_f
+		
 		puts "found: " + found.to_s
 		puts "error: " + error.to_s
+		puts "rappel: " + rappel.to_s
+		puts "precision: " + precision.to_s
 		
 		
-		return Struct::ComparedQrel.new(1, 1)
+		return Struct::ComparedQrel.new(rappel, precision)
 	end
 end #Connector
 
