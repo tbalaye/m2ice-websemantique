6cec87e38645fdb1a9a7a3079adf6535f1c21802
thibault.balaye <thibault.balaye@gmail.com>
2013-03-28 23:31:55 +0100

    Prise en compte des nouveaux paramètres transmis par le web service. Nouvelle mise en forme des résultats Ajout d'une fonctionnalité de mise en valeurs des termes recherchés dans les résultats.

--- Code/Randopedia/Randopedia/JSONObjects/ResultLine.cs
+++ Code/Randopedia/Randopedia/JSONObjects/ResultLine.cs
@@ -8,6 +8,7 @@ namespace Randopedia.JSONObjects
     public class ResultLine
     {
         public string Xpath  { get; set; }
+        public Boolean found { get; set; }
         public string Content { get; set; }
         public string pathFile { get; set; }
         public double weight { get; set; }
